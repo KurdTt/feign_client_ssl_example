46249bc54ac0bf7f74f8f7d5e7e278bccf1d4e56
Przemysław Książek <p.ksiazek@visiona.pl>
2022-04-16 15:07:56 +0200

    Add spring security to info and status

---
+++ server/info/src/main/java/com/example/feign/server/info/SecurityConfig.java
@@ -0,0 +1,30 @@
+/*
+ * This code is unpublished proprietary trade secret of
+ * Visiona Sp. z o.o., ul. Życzkowskiego 14, 31-864 Kraków, Poland.
+ *
+ * This code is protected under Act on Copyright and Related Rights
+ * and may be used only under the terms of license granted by
+ * Visiona Sp. z o.o., ul. Życzkowskiego 14, 31-864 Kraków, Poland.
+ *
+ * Above notice must be preserved in all copies of this code.
+ */
+
+package com.example.feign.server.info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+                .build();
+    }
+
+}
