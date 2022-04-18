00536d5233fa123a8148ee210e1a9f08ec97e13f
Przemysław Książek <p.ksiazek@visiona.pl>
2022-04-18 12:36:28 +0200

    Add separate truststores, fix key generator

--- generate_keys.bat
+++ generate_keys.bat
@@ -9,9 +9,23 @@ keytool -genkeypair -alias fakealias -storepass changeit -keypass changeit -keys
 keytool -delete -alias fakealias -storepass changeit -keystore certs.p12
 
 :: Import cert from info
-keytool -export -keystore info.p12 -alias info -file infoCert.crt
-keytool -importcert -file infoCert.crt -alias info -keystore certs.p12 -storepass changeit
+keytool -export -keystore info.p12 -alias info -file infoCert.crt -storepass changeit
+keytool -importcert -file infoCert.crt -alias info -keystore certs.p12 -storepass changeit -noprompt
 
 :: Import cert from status
-keytool -export -keystore status.p12 -alias status -file statusCert.crt
-keytool -importcert -file statusCert.crt -alias status -keystore certs.p12 -storepass changeit
+keytool -export -keystore status.p12 -alias status -file statusCert.crt -storepass changeit
+keytool -importcert -file statusCert.crt -alias status -keystore certs.p12 -storepass changeit -noprompt
+
+:: Create truststore only with cert info
+keytool -genkeypair -alias fakealias -storepass changeit -keypass changeit -keystore infocert.p12 -dname "CN=Developer, OU=Department, O=Company, L=City, ST=State, C=CA"
+keytool -delete -alias fakealias -storepass changeit -keystore infocert.p12
+keytool -importcert -file infoCert.crt -alias info -keystore infocert.p12 -storepass changeit -noprompt
+
+:: Create truststore only with cert info
+keytool -genkeypair -alias fakealias -storepass changeit -keypass changeit -keystore statuscert.p12 -dname "CN=Developer, OU=Department, O=Company, L=City, ST=State, C=CA"
+keytool -delete -alias fakealias -storepass changeit -keystore statuscert.p12
+keytool -importcert -file statusCert.crt -alias status -keystore statuscert.p12 -storepass changeit -noprompt
+
+:: Delete unnecessary files
+del statusCert.crt
+del infoCert.crt
